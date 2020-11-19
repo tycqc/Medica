e0ba57357c88644b996e896a14d18917fb304e7b
runoob-beggar <50787749+runoob-beggar@users.noreply.github.com>
2020-11-19 23:05:19 +0800

    改界面

--- medical v0.3/static/js/staff.js
+++ medical v0.3/static/js/staff.js
@@ -1,11 +1,12 @@
-function changbackgroundcolor() {
-    var trs = document.getElementById("staff_table").getElementsByTagName("tr");
-    for (var i = 0; i < trs.length; i++) {
-        if (i % 2 == 0) {
-            trs[i].style.backgroundColor = "#FFFFFF";
-        }
-        else {
-            trs[i].style.backgroundColor = "#000000";
+// $(function() {
+    function changbackgroundcolor() {
+        var trs = document.getElementById("staff_table").getElementsByTagName("tr");
+        for (var i = 0; i < trs.length; i++) {
+            if (i % 2 == 0) {
+                trs[i].style.backgroundColor = "#f9f9f9";
+            } else {
+                trs[i].style.backgroundColor = "#f7f7f7";
+            }
         }
     }
-}
+// })
